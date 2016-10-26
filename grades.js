c110fe0c60ae49c148e3a5f72178748c11ea02a2
Shawn M <forgeforza@outlook.com>
2016-10-25 23:30:20 -0500

    intial commit

---
+++ grades.js
@@ -0,0 +1,36 @@
+	var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+	var lowToHigh = scores.sort()
+
+
+	var gradeContainer = ""; 
+
+
+	//console.log("grades in order", lowToHigh);
+
+	var arrGrades = lowToHigh.toString()
+
+	//console.log("stringy grade", arrGrades);
+
+	for (i = 0; i < lowToHigh.length; i++) {
+			var lowOut = lowToHigh[0]
+			var highOut = lowToHigh[11]
+			var  gradeA = "";
+	
+		if (i >= 91){
+			 gradeA.push[10,11]
+		}
+
+			gradeContainer += lowToHigh[i] + " "
+	};
+
+
+
+
+
+	console.log(gradeA);
+
+
+
+	//console.log("How many of each grade", grades);
+	//console.log("What is the lowest grade?", lowOut);
+	// console.log ("What is the the highest grade?", highOut);
